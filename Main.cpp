eeae1a7da26fc90732635664930a788780e97417
Ofir408 <33639234+Ofir408@users.noreply.github.com>
2017-12-06 20:14:49 +0200

    Upload server.h/cpp + Main.cpp of the server
    I think that there is a problem when reading to the buffer. It looks like it's not empty, need to think about that.

---
+++ Main.cpp
@@ -0,0 +1,47 @@
+/*
+ * Main.cpp
+ *
+ *  Created on: Dec 2, 2017
+ *      Author: Ofir Ben Shoham
+ */
+#include <stdio.h>
+#include <iostream>
+#include <fstream>
+#include <string.h>
+#include <stdlib.h>
+#include "Server.h"
+
+
+using namespace std;
+
+int main() {
+
+	int portNumber;
+	string line;
+	ifstream myfile("exe/ConnectingDetails.txt");
+
+	if (!myfile.is_open()) {
+		cout << "Unable to open file";
+		return -1;
+	}
+	// Otherwise, opened the file.
+
+	getline(myfile, line); // no use of the ip adress in the server.
+
+	getline(myfile, line);
+	size_t posPort = line.find(":") + 1;
+	if (posPort > 0) {
+		string temp = line.substr(posPort);
+		portNumber = atoi(temp.c_str());
+	}
+	myfile.close();
+
+	Server server(portNumber); // the port we read from the file.
+	server.socketInitialize();
+	server.setAndStartServer();
+	server.workWithClients();
+
+
+	return 0;
+}
+
