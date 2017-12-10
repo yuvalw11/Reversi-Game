0bf7b8a62706e6b806f89e0a8c10c8d4849dd80d
Ofir408 <33639234+Ofir408@users.noreply.github.com>
2017-12-11 00:34:26 +0200

    Adding coding style comments

--- Client.h
+++ Client.h
@@ -15,8 +15,17 @@ namespace std {
 
 class Client {
 public:
+	/**
+	 * constructor for the client obejct.
+	 * Gets:
+	 * const char *serverIP - ip adress.
+	 * int serverPort - number of the port of the server, which is common both for the client & the server.
+	 */
 	Client(const char *serverIP, int serverPort);
 
+	/**
+	 * default constructor.
+	 */
 	Client() {
 		DoubleCell d;
 		clientSocket = 0;
@@ -25,13 +34,45 @@ public:
 		playerNum = 0;
 		turnsToPlay = d;
 	}
+
+	/**
+	 * gets boolean to know if it's the first time of connection in order to wait to the second client.
+	 * Then, connect to the socket of the server in order to pass data between the client
+	 * and the server.
+	 */
 	void connectToServer(bool firstClient = false);
+
+	/**
+	 * send and write with the socket of the server, and returns DoubleCell
+	 * object which defines to hold two cells to play.
+	 * The first is other cell and the second it's the current cell to play.
+	 */
 	DoubleCell sendAndWriteToServer(Board* board);
+
+	/**
+	 * read the client num of this current client, which is 1 for player number 1
+	 * Or 2 for player number 2. Then, set it as a field of the client.
+	 */
 	void readHisCilentNum();
+
+	/**
+	 * read from the socket of the server the next move to play, and return it.
+	 */
 	Cell getCurrentTurn();
+
+	/**
+	 * gets row and col and then send them into the socket of our server.
+	 */
 	void sendTurnToServer(int row, int col);
-	void startClient();
+
+	/**
+	 * return the cell to play of this current client and not the other one.
+	 */
 	Cell getMyCell() { return this->turnsToPlay.getCurrentPlayerMove(); }
+
+	/**
+	 * return other cell to play.
+	 */
 	Cell getOtherCell() { return this->turnsToPlay.getOtherPlayerMove(); }
 
 	int getPlayerNum() const {
--- Main.cpp
+++ Main.cpp
@@ -2,109 +2,47 @@
  * Main.cpp
  *
  *  Created on: Dec 2, 2017
- *      Author: Ofir Ben Shoham
+ *     Names : Yuval Weinstein & Ofir Ben Shoham.
+ *     Id: 208580613 & 208642496.
  */
 #include <stdio.h>
 #include <iostream>
 #include <fstream>
 #include <string.h>
 #include <stdlib.h>
-#include "Client.h"
-#include "temp/Board.h"
-#include "temp/GameRunner.h"
-#include "temp/HumanPlayer.h"
-#include "RemotePlayer.h"
+#include "Server.h"
+
 
 using namespace std;
 
 int main() {
 
-	Board b;
-	int userChoice = GameRunner::menu();
-	if (userChoice == 1 || userChoice == 2) {
-		HumanPlayer humanPlayer('X'); // the first player is human player.
-		HumanPlayer secondPlayer('O');
-		AIPlayer aiSecondPlayer('O');
-		char startPlayerSign = humanPlayer.getPlayerSign();
-		GameRunner gr(b, humanPlayer, secondPlayer, startPlayerSign);
+	int portNumber;
+	string line;
+	ifstream myfile("exe/ConnectingDetails.txt");
 
-		if (userChoice == 2) {
-			gr.setSecondPlayer(aiSecondPlayer); // set the second player to AI.
-		}
-
-		b.printBoard();
-
-		try {
-
-			gr.run(); // run the game.
-		} catch (exception *e) {
-			cout << "exception was caught :( \n";
-		}
-
-	} else {
-
-		// remote player
-		// first read the data from the file
-		const char* ipAdress;
-		int portNumber;
-		string line;
-		ifstream myfile("exe/ConnectingDetails.txt");
-
-		if (!myfile.is_open()) {
-			cout << "Unable to open file";
-			return -1;
-		}
-		// Otherwise, opened the file.
-
-		getline(myfile, line);
-		size_t posIP = line.find(":") + 1;
-		if (posIP > 0) {
-
-			string temp = line.substr(posIP);
-			char *cstr = new char[temp.length() + 1];
-			strcpy(cstr, temp.c_str());
-			char temp2[10];
-			strcpy(temp2, cstr);
-			ipAdress = temp2;
-			delete[] cstr;
-		}
-		getline(myfile, line);
-		size_t posPort = line.find(":") + 1;
-		if (posPort > 0) {
-			string temp = line.substr(posPort);
-			portNumber = atoi(temp.c_str());
-		}
-		myfile.close();
-
-		// create the remote player
-		Client firstClient(ipAdress, portNumber);
-		firstClient.connectToServer(true); // return the true
-		Board b2;
-
-		GameLogic gameLogic(b);
-		RemotePlayer firstRemotePlayer(firstClient, b); // first player.
-
-		firstRemotePlayer.setPlayerSign(firstRemotePlayer.getPlayerSign());
-
-		GameRunner gr(b, firstRemotePlayer, firstRemotePlayer,
-				firstRemotePlayer.getPlayerSign());
-		b.printBoard();
-
-		// run the game for the remote players
-	while (true) {
-		try {
-			gr.run(); // run the game.
-			//firstClient.sendAndWriteToServer();
-		} catch (exception *e) {
-			cout << "exception was caught :( \n";
-		} catch(char const* msg) {
-			cout << msg;
-		}
-
+	if (!myfile.is_open()) {
+		cout << "Unable to open file";
+		return -1;
 	}
+	// Otherwise, opened the file.
 
-}
+	getline(myfile, line); // no use of the ip adress in the server.
 
-return 0;
+	getline(myfile, line);
+	size_t posPort = line.find(":") + 1;
+	if (posPort > 0) {
+		string temp = line.substr(posPort);
+		portNumber = atoi(temp.c_str());
+	}
+	myfile.close();
+
+	Server server(portNumber); // the port we read from the file.
+	server.socketInitialize();
+	server.setAndStartServer();
+	server.workWithClients();
+
+
+	return 0;
 }
 
